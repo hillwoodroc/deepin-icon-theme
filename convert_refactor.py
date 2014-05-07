7c51339aad806c6bc826ccd39dbc8c49bda1d7fc
Zhang Yueqian <nohappiness@gmail.com>
2014-05-07 09:09:28 +0800

    Update script to make it work properly.

--- convert_refactor.py
+++ convert_refactor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+
+import os
+import sys
+import ConfigParser
+
 class PngGenerate(object):
 
     def __init__(self, theme_dir, src_dir):
@@ -12,6 +17,7 @@ class PngGenerate(object):
 
 
     def _init_config(self):
+        # Read needed vars in index.theme file.
         self.config = ConfigParser.ConfigParser()
         self.config.read(self.config_file)
         self.directories = self.config.get('Icon Theme', 'Directories').split(',')
@@ -19,6 +25,7 @@ class PngGenerate(object):
 
 
     def _init_dirs(self):
+        # Initialize direcotries variable.
         self.icon_dirs = {}
         # check if directory in format '16x16/apps'
         if self.directories[0].split('/')[0][0].isdigit():
@@ -28,35 +35,36 @@ class PngGenerate(object):
 
         for directory in self.directories:
             if self.digit_first:
-                size = i.split('/')[0]
-                dir_type = i.split('/')[1]
+                size = directory.split('/')[0]
+                icon_type = directory.split('/')[1]
             else:
-                size = i.split('/')[0]
-                dir_type = i.split('/')[1]
-            if not self.icon_dirs.has_key(dir_type):
-                self.icon_dirs[dir_type] = set()
-            self.icon_dirs[icon_type].add(size)
+                size = directory.split('/')[1]
+                icon_type = directory.split('/')[0]
+            size = size.split('x')[0]
+
+            if not self.icon_dirs.has_key(icon_type):
+                self.icon_dirs[icon_type] = []
+
+            if not (size, directory) in self.icon_dirs[icon_type]:
+                self.icon_dirs[icon_type].append((size,directory))
 
 
     def _generate_png(self, image_path, icon_type):
-        image_name = os.path.basename(image_path)
-        for size in self.icon_dirs[icon_type]:
+        # Convert given image into given type_dir.
+        image_name = "%s.png" % os.path.basename(image_path)[:-4]
+        for size,directory in self.icon_dirs[icon_type]:
             if size == 'scalable':
                 continue
-            length = size.split('x')[0]
-            if self.digit_first:
-                output_dir = os.path.join(self.theme, "%sx%s" % (length, length), icon_type)
-            else:
-                output_dir = os.path.join(self.theme, icon_type, "%sx%s" % (length, length))
 
-            output_file = os.path.join(output_dir, image_name)
-            PngGenerate.svg_to_png(image_path, length, output_file)
+            output_file = os.path.join(self.theme, directory, image_name)
+            PngGenerate.svg_to_png(image_path, size, output_file)
 
 
     def convert(self):
+        # Main method to convert svg files in src_dir.
         for directory in os.listdir(self.src):
             current_path = os.path.join(self.src, directory)
-            for svg_file in os.listdir(directory):
+            for svg_file in os.listdir(current_path):
                 image_path = os.path.join(current_path, svg_file)
                 if not svg_file.endswith(".svg"):
                     continue
@@ -66,16 +74,31 @@ class PngGenerate(object):
 
 
     @classmethod
-    def svg_to_png(input_file, size, output_file):
+    def svg_to_png(cls, input_file, size, output_file):
+        # Convert svg to png file using command:
+        # inkscape -f input_file -w width -e output_file
         dir_name = os.path.dirname(output_file)
 
         if not os.path.exists(dir_name):
             os.makedirs(dir_name)
 
-        os.system("inkscape -f %s -e %s -w %s" % (input_file, size, output_file))
+        os.system("inkscape -f %s -w %s -e %s" % (input_file, size, output_file))
 
 
 
 if __name__ == "__main__":
-    converter = PngGenerate('Deepin', 'src')
+    if not len(sys.argv) == 3:
+        print "Usage: %s Theme_dir Svg_dir" % __file__
+        exit()
+
+    theme_dir = sys.argv[1]
+    src_dir = sys.argv[2]
+    if not os.path.exists(src_dir):
+        print "%s not exists." % src_dir
+        exit()
+    if not os.path.exists(theme_dir):
+        print "%s not exists." % theme_dir
+        exit()
+
+    converter = PngGenerate(theme_dir, src_dir)
     converter.convert()
